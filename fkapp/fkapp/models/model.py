10b25e270d8ed22df87786c1b68ffb8701f44d84
ajaxj <ajaxj@qq.com>
2012-08-21 18:13:27 +0800

    bootstrap2.1

--- fkapp/fkapp/models/model.py
+++ fkapp/fkapp/models/model.py
@@ -1,9 +1,27 @@
 # -*- coding:utf-8
-from flaskext.couchdb import Document ,TextField,IntegerField
+from flaskext.couchdb import Document ,TextField,IntegerField, ViewField, ViewDefinition,DateTimeField
+
+import datetime
 
 
 __author__ = 'window2003@gmail.com'
 
+
+
+# model
+class Signature(Document):
+    doc_type = 'signature'
+
+    message = TextField()
+    author = TextField()
+    time = DateTimeField(default=datetime.datetime.now)
+
+    all = ViewField('guestbook', '''
+        function (doc) {
+            if (doc.doc_type == 'signature') {
+                emit(doc.time, doc);
+            };
+        }''')
 
 
 class User(Document):
@@ -12,6 +30,15 @@ class User(Document):
     password = TextField()
     nickname = TextField()
     level = IntegerField()
+
+    all = ViewField('blog', '''\
+    function (doc) {
+        if (doc.doc_type == 'user') {
+
+                emit(null, doc);
+
+        };
+    }''')
 
 
 
--- fkapp/fkapp/views/admin.py
+++ fkapp/fkapp/views/admin.py
@@ -2,12 +2,14 @@
 from flask import Module,render_template,request,flash,redirect
 
 #使用couchdb-ext
-from fkapp.models.model import User,Shop
+from fkapp.models.model import User,Shop,Signature
 
 #关掉couchdb-kit
 #from fkapp.models.greeting import User
 
 from fkapp.forms import LoginForm,SignupForm
+from flaskext.couchdb import paginate
+
 
 __author__ = 'Administrator'
 
@@ -18,16 +20,16 @@ admin = Module(__name__)
 def index():
     return render_template('admin/index.html')
 
-@admin.route("/users",methods=('GET','POST'))
-def users():
-    if request.method == 'POST':
-        _username = request.form['username']
-        _password = request.form['password']
-        user = User(username=_username,password=_password)
-        user.save()
-        return render_template('admin/users.html')
-    else:
-        return render_template('admin/users.html')
+#@admin.route("/users",methods=('GET','POST'))
+#def users():
+#    if request.method == 'POST':
+#        _username = request.form['username']
+#        _password = request.form['password']
+#        user = User(username=_username,password=_password)
+#        user.save()
+#        return render_template('admin/users.html')
+#    else:
+#        return render_template('admin/users.html')
 
 
 @admin.route("/login/", methods=("GET","POST"))
@@ -47,6 +49,24 @@ def login():
 
 
 
+#user
+@admin.route("/users",methods=('GET','POST'))
+def users():
+    form = SignupForm(next=request.args.get('next',None))
+    if form.validate_on_submit():
+        _username = form.username.data
+        _password = form.password.data
+        _nickname = form.nickname.data
+        user = User(username=_username,password=_password,nickname = _nickname,level = 1)
+        user.store()
+        return redirect('admin/users')
+    else:
+    #flash("back,errors")
+        return render_template('admin/users.html',form = form)
+
+
+
+
 #注册用户
 @admin.route("/signup/",methods=("GET","POST"))
 def signup():
@@ -57,15 +77,26 @@ def signup():
         _nickname = form.nickname.data
         user = User(username=_username,password=_password,nickname = _nickname,level = 1)
         user.store()
-        return redirect('users')
+        return redirect('/admin/users')
     else:
     #flash("back,errors")
         return render_template('admin/signup.html',form = form)
 
 
+
+#Shop
+
+@admin.route("/shops/")
+def shops():
+    return render_template('admin/shops.html')
+
+
 @admin.route("/test")
 def test():
-    user = User(username="u1",password="p1",nickname="n1")
-    user.store()
-    return render_template('admin/users.html')
+    signature = Signature(message='message', author='author')
+    signature.store()
+    print Signature.all()
+#    page = paginate(Signature.all(), 5, request.args.get('start'))
+#    print page
+    return render_template('admin/shops.html')
 
