32f4d7d1f9a59dd9498ad2069d47b108f1eb0ba9
ajaxj <ajaxj@qq.com>
2012-11-14 17:58:59 +0800

    添加了admin.py category,item table

--- flaskweb/mysite/views/admin.py
+++ flaskweb/mysite/views/admin.py
@@ -1,8 +1,12 @@
 # -*- coding:utf-8 -*-
+from mysite.models.model import Category,Item
+from mysite.utils.fetch import foo
+from mysite.extensions import db
+
 __author__ = 'window2003@gmail.com'
 
 
-from flask import Module,redirect,url_for
+from flask import Module,redirect,url_for,render_template,current_app
 
 __author__ = 'window2003@gmail.com'
 
@@ -10,12 +14,33 @@ admin = Module(__name__)
 
 @admin.route('/')
 def index():
-    return "this is admin"
+    return render_template('admin/index.html')
 
+#分类列表
 @admin.route('/categorylist')
 def categorylist():
-    return "this is category list"
+#    print foo()
+    #打印log
+#    current_app.logger.debug("this is debug")
+#    current_app.logger.error("this is error")
+    _category = Category.query.all()
+    print _category
+    _item = Item.query.all()
+    print _item
+    return render_template('admin/categorylist.html')
 
+#数据列表
 @admin.route('/itemlist')
 def itemlist():
-    return "this is items list"
+    return render_template('admin/itemlist.html')
+
+
+# 初始首页跳转
+@admin.route('/initdata')
+def initdata():
+#    _category = Category(name="n1")
+    _item = Item(title="t1")
+#    db.session.add(_category)
+    db.session.add(_item)
+    db.session.commit()
+    return redirect(url_for('index'))
